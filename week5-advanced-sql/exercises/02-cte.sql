981f824582edbb1a76c9b9af98c436d306f86236
Uygar KOSE <osmanuygar.kose@sahibinden.com>
2025-10-03 12:21:04 +0300

    Add Common Table Expressions (CTE) exercises to 02-cte.sql

---
+++ week5-advanced-sql/exercises/02-cte.sql
@@ -0,0 +1,73 @@
+-- Week 5 - Common Table Expressions (CTE) Exercises
+
+/*
+ALIŞTIRMA 1: Basic CTE
+CTE kullanarak müşteri başına toplam sipariş tutarını hesapla
+ve 1000 TL'den fazla harcayan müşterileri listele
+*/
+-- Kodunuzu buraya yazın
+
+
+
+/*
+ALIŞTIRMA 2: Multiple CTEs
+İki CTE oluştur:
+1) Müşteri bazında toplam harcama
+2) Ürün bazında toplam satış
+Sonra ikisini JOIN et
+*/
+-- Kodunuzu buraya yazın
+
+
+
+/*
+ALIŞTIRMA 3: Recursive CTE - Kategori Hiyerarşisi
+Kategori ağacını recursive CTE ile listele
+Her seviye için indent göster
+*/
+-- Kodunuzu buraya yazın
+
+
+
+/*
+ALIŞTIRMA 4: Recursive CTE - Employee Hierarchy
+Çalışan hiyerarşisini göster (manager chain)
+*/
+-- Kodunuzu buraya yazın
+
+
+
+/*
+ALIŞTIRMA 5: CTE with Window Functions
+CTE içinde window function kullan
+Her müşterinin son 3 siparişini bul
+*/
+-- Kodunuzu buraya yazın
+
+
+
+/*
+ALIŞTIRMA 6: Recursive CTE - Number Series
+1'den 10'a kadar sayıları recursive CTE ile üret
+*/
+-- Kodunuzu buraya yazın
+
+
+
+/*
+ALIŞTIRMA 7: Complex CTE Chain
+3 CTE oluştur:
+1) Aktif müşteriler (son 90 günde sipariş verenler)
+2) Bu müşterilerin toplam harcamaları
+3) Segment bazında average hesapla
+*/
+-- Kodunuzu buraya yazın
+
+
+
+/*
+ALIŞTIRMA 8: CTE ile Data Transformation
+CTE kullanarak sipariş verilerini pivot et
+Her ayın toplam satışını göster
+*/
+-- Kodunuzu buraya yazın
